dafe80386f1ab7f76095a33f2e9da5f5107a47d7
Charles Wu <charleswu.01@gmail.com>
2020-02-12 16:47:38 -0500

    updated classifier file

--- Preprocessing/Classifier.py
+++ Preprocessing/Classifier.py
@@ -33,77 +33,81 @@ Mastoids = ['M1','M2']
 EOG_list = ['HEOG', 'VEOG']
 n_eeg_channels = 32
 
-l_freq = 0.1
-h_freq = 110.0
-notch_freq = [60.0,120.0]
-fname_suffix = "filter_%d_%dHz_notch_raw" %(l_freq, h_freq)
-alpha = 15
-event_dir = tmp_rootdir + "event_files/"
-event_id = {'S1/MU2/CON': 120,
-            'S1/MU2/TAR': 121,
-            'S1/ZHUO2/CON': 140, 
-            'S1/ZHUO2/TAR': 141, 
-            'S3/MU7/CON': 320,
-            'S3/MU7/TAR': 321,
-            'S3/ZHUO7/CON': 340, 
-            'S3/ZHUO7/TAR': 341,
-            'S5/MU5/CON': 520,
-            'S5/MU5/TAR': 521, 
-            'S5/ZHUO5/CON':540,
-            'S5/ZHUO5/TAR':541,
-            'S6/MU4/CON':620,
-            'S6/MU4/TAR':621,
-            'S6/ZHUO4/CON':640,
-            'S6/ZHUO4/TAR':641}
-events ='S1/MU2/CON','S1/MU2/TAR','S1/ZHUO2/CON', 'S1/ZHUO2/TAR', 'S3/MU7/CON','S3/MU7/TAR','S3/ZHUO7/CON','S3/ZHUO7/TAR','S5/MU5/CON','S5/MU5/TAR', 'S5/ZHUO5/CON','S5/ZHUO5/TAR','S6/MU4/CON','S6/MU4/TAR','S6/ZHUO4/CON','S6/ZHUO4/TAR'
 
-subj_list = ["S01", "S02", "S03", "S04", "S05", "S06", "S07", "S09", "S10", "S11", "S12", "S14", "S15"]
-session_list = ['PRE','POST']
+subj_list = ['001', '002', '003', '004', '005', '008', '009', '011']
+n_subj = len(subj_list) 
+eeg_chan = []
+eeg_chan = eeg_chan + ['A' + str(i+1) for i in range(32)]
+chnames = eeg_chan + ['EXG' + str(i+1) for i in range(5)]
+blocks = ['can', 'rev']
+stimuli = ['standard', 'deviant']
+biosemi_layout = mne.channels.read_montage(tmp_rootdir + 'biosemi_cap_32_M_2_EOG_3.locs')
+biosemi_layout.ch_names = chnames
+event_id = {'standard/can':65321,
+            'deviant/can':65322,
+            'standard/rev':65341,
+            'deviant/rev':65342,
+            'can/beer':65391,
+            'can/pier':65392,
+            'canTest1':65401,
+            'canTest2':65402,
+            'rev/beer':65491,
+            'rev/pier':65492,
+            'revTest1':65501,
+            'revTest2':65502
+            }
+
 n_subj = len(subj_list) 
 n_sesh = len(session_list)
 ##read the events from the event files instead of the stim channels in the data.
-for j in range(n_sesh):
-    sesh = session_list[j]
-    classify_scores = np.empty((0,701))
-    for i in range(n_subj):
-        ch_names = ['FZ', 'CZ', 'PZ']
-        subj = subj_list[i]
-        fname = word_epoch_dir + "%s_%s_%s_word-epo.fif" %(subj, sesh, fname_suffix)
-        this_epoch = mne.read_epochs(fname, preload = True)
-        epochs_classify = this_epoch[events].crop(-0.2, 0.5)
-        CZ = epochs_classify['S6/ZHUO4/TAR'].ch_names.index('CZ')
-        FZ = epochs_classify['S6/ZHUO4/TAR'].ch_names.index('FZ')
-        PZ = epochs_classify['S6/ZHUO4/TAR'].ch_names.index('PZ')
-        
-        selected_data = epochs_classify.get_data()[:,[CZ,FZ,PZ],:]
 
-        classify_event = []
-        for x in map(str,epochs_classify.events[:,2]):
-            classify_event.append(int(x[2]))
+n_subj = len(subj_list) 
+#============================================    
+for i in range(n_subj):
+    subj = subj_list[i]
 
-        X = selected_data
-        y = classify_event
-        clf = make_pipeline(StandardScaler(), LogisticRegression())
-        time_decod = SlidingEstimator(clf, n_jobs=1, scoring='roc_auc')
+    resampled_fname = resampled_dir + "%s_resampled_raw.fif" %(subj)
+    
+    raw = mne.io.read_raw_fif(resampled_fname, preload = True)
+    events = mne.find_events(raw)
 
-        scores = cross_val_multiscore(time_decod, X, y, cv=5, n_jobs=1)
-        # Mean scores across cross-validation splits
-        scores = np.mean(scores, axis=0)
-        classify_scores = np.append(classify_scores, [scores], axis = 0)
-        score_name = classify_dir + "%s.txt" %(sesh)
-        np.savetxt(score_name, classify_scores, fmt = '%1.4f')
-        # Plot
-        fig, ax = plt.subplots()
+    raw.set_eeg_reference('average', projection = True)
 
-        ax.plot(epochs_classify.times, scores, label='score')
-        ax.axhline(.5, color='k', linestyle='--', label='chance')
-        ax.set_xlabel('Times')
-        ax.set_ylabel('AUC')  # Area Under the Curve
-        ax.legend()
-        ax.axvline(.0, color='k', linestyle='-')
-        ax.set_title('Sensor space decoding')
-        tmp_fig_name = fig_dir + "%s_%s.png" %(subj, sesh)
-        fig.savefig(tmp_fig_name)
+    baseline = (-0.2, 0.0)
+    epochs = mne.Epochs(raw, events = events, event_id=event_id, 
+                        tmin = -0.2, tmax = 0.5, baseline=baseline)
+
+    FZ = epochs['S6/ZHUO4/TAR'].ch_names.index('FZ')
+    
+    selected_data = epochs_classify.get_data()[:,[FZ],:]
+
+    classify_event = []
+    for x in map(str,epochs_classify.events[:,2]):
+        classify_event.append(int(x[2]))
+
+    X = selected_data
+    y = classify_event
+    clf = make_pipeline(StandardScaler(), LogisticRegression())
+    time_decod = SlidingEstimator(clf, n_jobs=1, scoring='roc_auc')
+
+    scores = cross_val_multiscore(time_decod, X, y, cv=5, n_jobs=1)
+    # Mean scores across cross-validation splits
+    scores = np.mean(scores, axis=0)
+    classify_scores = np.append(classify_scores, [scores], axis = 0)
+    score_name = classify_dir + "%s.txt" %(sesh)
+    np.savetxt(score_name, classify_scores, fmt = '%1.4f')
+    # Plot
+    fig, ax = plt.subplots()
+
+    ax.plot(epochs_classify.times, scores, label='score')
+    ax.axhline(.5, color='k', linestyle='--', label='chance')
+    ax.set_xlabel('Times')
+    ax.set_ylabel('AUC')  # Area Under the Curve
+    ax.legend()
+    ax.axvline(.0, color='k', linestyle='-')
+    ax.set_title('Sensor space decoding')
+    tmp_fig_name = fig_dir + "%s_%s.png" %(subj, sesh)
+    fig.savefig(tmp_fig_name)
       # size = np.array(range(epoch_size))
       # dataframe = pd.DataFrame(np.repeat([[subj],[sesh],[word]],FZ.shape[0], axis=1).T)
       # dataframe['Trial'] = np.repeat(size + 1, time)
