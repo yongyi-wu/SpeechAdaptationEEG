15484457bfda60c60ec641713381c9beedf0a233
Charles Wu <charleswu.01@gmail.com>
2020-03-27 10:17:31 -0400

    cleaned the visualize file

--- Preprocessing/Preprocessing1_visualize.py
+++ Preprocessing/Preprocessing1_visualize.py
@@ -14,6 +14,8 @@ raw_dir = config.raw_dir
 resampled_dir = config.resampled_dir
 filtered_dir = config.filtered_dir
 fig_dir_mmn = config.fig_dir_mmn
+fig_dir_test = config.fig_dir_test
+fig_dir_exposure = config.fig_dir_exposure
 
 ###################################################################
 ######################### subject #################################
@@ -24,7 +26,6 @@ fig_dir_mmn = config.fig_dir_mmn
 can_rev = config.can_rev
 rev_can = config.rev_can
 subj_list = config.subj_list
-n_subj = len(subj_list) 
 
 
 ###################################################################
@@ -45,68 +46,79 @@ print('drop_names = ', drop_names)
 
 event_id = config.event_id
 
-
-colors = dict(Can="Crimson", Rev="CornFlowerBlue")
-linestyles = dict(Standard='-', Deviant='--')
-
+Block = config.Block
+pick_Exposure = config.Exposure
+pick_Test = config.Test
+pick_MMN = config.MMN
 chan = 'A31'
-
-MMN = config.MMN
 
 biosemi_layout = mne.channels.read_montage(tmp_rootdir + 'biosemi_cap_32_M_2_EOG_3.locs')
 biosemi_layout.ch_names = eeg_chan
 
-evoked_subj = dict()
-evoked = dict()
+def plot_indiv_ERP(eventType1, eventType2, picks):
+    event_pick, colors, linestyles = helper.pick_event_type(eventType1, eventType2)
+    evoked_subj = dict()
+    evoked = dict()
+    n_subj = len(subj_list) 
 
-n_subj = len(subj_list) 
-#============================================    
-for i in range(n_subj):
-    subj = subj_list[i]
-    print(subj)
-    resampled_fname = resampled_dir + "%s_resampled_raw.fif" %(subj)
-    
-    raw = mne.io.read_raw_fif(resampled_fname, preload = True)
-    events = mne.find_events(raw, shortest_event = 1)
-    raw.pick_types(include = eeg_chan, exclude = raw.info['bads'])
-
-    raw.filter(0.1, 32)
-    raw.set_eeg_reference('average', projection = False)
-    
-    #raw.filter(l_freq, h_freq)
+    if str(eventType2) == 'pick_MMN':
+        fig_dir = fig_dir_mmn
+        etype == 'MMN'
+    elif str(eventType2) == 'pick_Test':
+        fig_dir = fig_dir_test
+        etype == 'Test'
+    else:
+        fig_dir = fig_dir_exposure
+        etype == 'Exposure'
         
-    baseline = (-0.2, 0.0)
-    epochs = mne.Epochs(raw, picks = chan, events = events, event_id=event_id, 
-                        tmin = -0.2, tmax = 0.5, baseline=baseline)
+    #============================================    
+    for i in range(n_subj):
+        subj = subj_list[i]
+        print(subj)
+        resampled_fname = resampled_dir + "%s_resampled_raw.fif" %(subj)
+        
+        raw = mne.io.read_raw_fif(resampled_fname, preload = True)
+        events = mne.find_events(raw, shortest_event = 1)
+        raw.pick_types(include = eeg_chan, exclude = raw.info['bads'])
 
-    for j in range(len(MMN)):
-        evoked_subj[MMN[j]] = epochs[MMN[j]].average()
-        if i == 0:
-            evoked[MMN[j]] = [evoked_subj[MMN[j]]]
-        else:
-            evoked[MMN[j]].append(evoked_subj[MMN[j]])
+        raw.filter(0.1, 32)
+        raw.set_eeg_reference('average', projection = False)
+            
+        baseline = (-0.2, 0.0)
+        epochs = mne.Epochs(raw, picks = picks, events = events, event_id=event_id, 
+                            tmin = -0.2, tmax = 0.5, baseline=baseline)
 
-    fig = mne.viz.plot_compare_evokeds(evoked_subj, 
+        for j in range(len(event_pick)):
+            evoked_subj[event_pick[j]] = epochs[event_pick[j]].average()
+            if i == 0:
+                evoked[event_pick[j]] = [evoked_subj[event_pick[j]]]
+            else:
+                evoked[event_pick[j]].append(evoked_subj[event_pick[j]])
+
+        fig = mne.viz.plot_compare_evokeds(evoked_subj, 
+                                     show_sensors = False,
+                                     picks = chan,
+                                     colors = colors,
+                                     linestyles = linestyles,
+                                     title = 'Subject_%s_%s'%(subj, picks),
+                                     show=False)
+        fig_savename = fig_dir + '%s_%s_%s.png' %(etype, subj, picks)
+        fig.savefig(fig_savename)
+
+    block_etype = dict()
+    for j in range(len(event_pick)):
+        block_etype[event_pick[j]] = mne.grand_average(evoked[event_pick[j]])
+
+    fig1 = mne.viz.plot_compare_evokeds(block_etype, 
                                  show_sensors = False,
                                  picks = chan,
                                  colors = colors,
                                  linestyles = linestyles,
-                                 title = 'Subject_%s_%s'%(subj, chan),
-                                 show=False)
-    fig_savename = fig_dir_mmn + '%s_MMN_%s.png' %(subj, chan)
-    fig.savefig(fig_savename)
+                                 title = 'Average_FZ',
+                                 show = False)
+    fig_savename = fig_dir + 'ave_%s_%s.png'%(etype, picks)
+    fig1.savefig(fig_savename)
 
-block_mmn = dict()
-for j in range(len(MMN)):
-    block_mmn[MMN[j]] = mne.grand_average(evoked[MMN[j]])
-
-fig1 = mne.viz.plot_compare_evokeds(block_mmn, 
-                             show_sensors = False,
-                             picks = chan,
-                             colors = colors,
-                             linestyles = linestyles,
-                             title = 'Average_FZ',
-                             show = False)
-fig_savename = fig_dir_mmn + 'ave_MMN_%s.png'%(chan)
-fig1.savefig(fig_savename)
+    if __name__ = '__main__':
+        plot_indiv_ERP()
 
