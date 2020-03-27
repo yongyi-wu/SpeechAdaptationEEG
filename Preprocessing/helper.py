c2c71a93fd601d05080f5089e9f92e6d4d2a66ec
Charles Wu <charleswu.01@gmail.com>
2020-03-27 10:26:12 -0400

    new functions in the helper file

--- Preprocessing/helper.py
+++ Preprocessing/helper.py
@@ -9,5 +9,16 @@ def join_events(eventType1, eventType2):
       for i in range(len(eventType1)):
             for j in range(len(eventType1)):
                   full_events.append('/'.join([eventType1[i], eventType2[j]]))
-      return(full_events)
+      return full_events
 
+
+def pick_event_type(eventType1, eventType2, e1color = 'Crimson', e2color = 'CornFlowerBlue', e1ltyp = '-', e2ltyp = '--'):
+	###Assuming eventType1 is always the block i.e., canonical and reverse
+	###eventype 1 will always differ in color and eventtype2 in linestyle
+	###default colors are set
+	###linestyle is fixed
+	event_list = join_events(eventType1, eventType2)
+	color = dict(eventType1[0]=e1color, eventType1[1] = e2color)
+	linestyles = dict(eventType2[0]=e1ltyp, eventtype2[1] = e2ltyp)
+
+	return event_list, color, linestyles
